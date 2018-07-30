86a9bd8feb78acd91fb508e1a88b84869a80c020
jacobrohan <jrohanjoseph@gmail.com>
2018-07-30 18:32:08 +0400

    Change keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,3 @@
-module.exports = {
-  mongoURI: 'mongodb://tutrdb:tutrdb123@ds257851.mlab.com:57851/tutrapp',
-  secret: 'sdfhskdjfhskdjbfskjvkjnvouhfpd'
-};
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
 } else {
